0c6bdeba8ef3bd0a9f1d812a7a12b0eb0a98d60c
Tom <8162045+tbjgolden@users.noreply.github.com>
2022-12-02 18:51:38 +0000

    chore: simplified regu

--- lib/regu/regu.test.ts
+++ lib/regu/regu.test.ts
@@ -4,4 +4,5 @@ test("escape", () => {
   expect(escape(`///`)).toBe("///");
   expect(escape(`\\`)).toBe("\\\\");
   expect(escape(`hello.world*`)).toBe("hello\\.world\\*");
+  expect(escape(`hello\n.world\t*`)).toBe("hello\\n\\.world\\t\\*");
 });
