357171ba90bc1e186e81901a9ca545c65521bc80
Tom <8162045+tbjgolden@users.noreply.github.com>
2022-12-02 19:29:28 +0000

    chore: refactor to use native url instead

--- lib/url/url.ts
+++ lib/url/url.ts
@@ -1,63 +1,6 @@
-import { URL } from "node:url";
-
 import { normalize } from "./url.helpers";
 export { normalize } from "./url.helpers";
 
-const DEFAULT_PORT_MAP = new Map<string, number>([
-  ["http:", 80],
-  ["https:", 443],
-  ["ws:", 80],
-  ["wss:", 443],
-  ["ftp:", 21],
-  ["ssh:", 22],
-  ["sftp:", 22],
-  ["mysql:", 3306],
-  ["postgresql:", 5432],
-  ["mongodb:", 27_017],
-  ["redis:", 6379],
-]);
-
-export const getDefaultPortForProtocol = (protocol: string): number | undefined => {
-  return DEFAULT_PORT_MAP.get(protocol);
-};
-
-export type ParsedURL = {
-  hash: string;
-  host: string;
-  hostname: string;
-  href: string;
-  origin: string;
-  password: string;
-  pathname: string;
-  protocol: string;
-  search: string;
-  searchParams: URLSearchParams;
-  username: string;
-  port: number | "";
-};
-
-export const parse = (inputURL: string, baseURL?: string): ParsedURL => {
-  const rawURL = new URL(inputURL, baseURL);
-  return {
-    hash: rawURL.hash,
-    host: rawURL.host,
-    hostname: rawURL.hostname,
-    href: rawURL.href,
-    origin: rawURL.origin,
-    password: rawURL.password,
-    pathname: rawURL.pathname,
-    protocol: rawURL.protocol,
-    search: rawURL.search,
-    searchParams: rawURL.searchParams,
-    username: rawURL.username,
-    port:
-      rawURL.port === ""
-        ? getDefaultPortForProtocol(rawURL.protocol) ?? ""
-        : Number.parseInt(rawURL.port),
-  };
-};
-
 export const resolve = (fromUrl: string, toUrl: string): string => {
-  const url = parse(toUrl, fromUrl);
-  return normalize(url.href);
+  return normalize(new URL(toUrl, fromUrl).href);
 };
