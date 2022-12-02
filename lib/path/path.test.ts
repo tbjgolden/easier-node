2a4f6b3a8249cf0450bde8ae0cfeb010aca2421f
Tom <8162045+tbjgolden@users.noreply.github.com>
2022-12-02 18:22:34 +0000

    chore: simplified path

--- lib/path/path.test.ts
+++ lib/path/path.test.ts
@@ -1,4 +1,14 @@
-import { ensureSlash, extension, join, normalize, resolve, split } from "./path";
+import {
+  ensureNoSlash,
+  ensureSlash,
+  extension,
+  join,
+  normalize,
+  relative,
+  resolve,
+  split,
+  up,
+} from "./path";
 
 test("join", () => {
   expect(join("a", "/b/c")).toBe("a/b/c");
@@ -10,6 +20,7 @@ test("resolve", () => {
   expect(resolve("/a", "/b/c")).toBe("/b/c");
   expect(resolve("a", "b/c")).toBe(process.cwd() + "/a/b/c");
   expect(resolve("a", "b/c/")).toBe(process.cwd() + "/a/b/c");
+  expect(resolve("a", "/b/c/")).toBe("/b/c");
 });
 
 test("normalize", () => {
@@ -18,6 +29,7 @@ test("normalize", () => {
   expect(normalize("/a/./b/")).toBe("/a/b");
   expect(normalize("a/..")).toBe(".");
   expect(normalize("a/../")).toBe(".");
+  expect(normalize("")).toBe(".");
 });
 
 test("split", () => {
@@ -29,6 +41,8 @@ test("split", () => {
 });
 
 test("readExtension", () => {
+  expect(extension("")).toBe("");
+  expect(extension("/")).toBe("");
   expect(extension("/hello/world")).toBe("");
   expect(extension("/hello/world.png")).toBe(".png");
   expect(extension("/hello/world.png", 2)).toBe(".png");
@@ -44,4 +58,34 @@ test("ensurePathEndsWithSlash", () => {
   expect(ensureSlash("/hello/world/")).toBe("/hello/world/");
   expect(ensureSlash("/hello")).toBe("/hello/");
   expect(ensureSlash("/")).toBe("/");
+});
+
+test("up", () => {
+  expect(up("/")).toBe("/");
+  expect(() => up("/", true)).toThrow();
+  expect(up("/hello/world/")).toBe("/hello");
+  expect(up("/hello/world")).toBe("/hello");
+  expect(up("/hello/")).toBe("/");
+  expect(up("/hello")).toBe("/");
+});
+
+test("relative", () => {
+  expect(relative("/", "/hello/world/")).toBe("hello/world");
+  expect(relative("/hello/world", "/hello/planet/")).toBe("../planet");
+  expect(relative("/hello/world", "/hello/planet")).toBe("../planet");
+  expect(relative("/hello/world/", "/hello/planet/")).toBe("../planet");
+  expect(relative("a", "b")).toBe("../b");
+  expect(relative("/hello", "/")).toBe("..");
+  expect(relative("/hello/a", "/hello/")).toBe("..");
+  expect(relative("/hello/world", "/hello/world/earth")).toBe("earth");
+  expect(relative("/hello", "/hello")).toBe("");
+  expect(relative("/hello/", "/hello")).toBe("");
+});
+
+test("ensureNoSlash", () => {
+  expect(ensureNoSlash("/", true)).toBe("");
+  expect(ensureNoSlash("/")).toBe("/");
+  expect(ensureNoSlash("/hello/world")).toBe("/hello/world");
+  expect(ensureNoSlash("/hello/world/", true)).toBe("/hello/world");
+  expect(ensureNoSlash("a")).toBe("a");
 });
--- lib/path/path.ts
+++ lib/path/path.ts
@@ -1,188 +1,24 @@
-const CHAR_DOT = 46;
-const CHAR_FORWARD_SLASH = 47;
+import path from "node:path";
 
-// Resolve . and .. elements in a path with directory names
-const normalizeString = (path: string) => {
-  const shouldAllowAboveRoot = path.codePointAt(0) === CHAR_FORWARD_SLASH;
-  let result = "";
-  let lastSegmentLength = 0;
-  let lastSlash = -1;
-  let dots = 0;
-  let code = 0;
-  for (let index = 0; index <= path.length; ++index) {
-    if (index < path.length) {
-      code = path.codePointAt(index) as number;
-    } else if (code === CHAR_FORWARD_SLASH) {
-      break;
-    } else {
-      code = CHAR_FORWARD_SLASH;
-    }
-
-    if (code === CHAR_FORWARD_SLASH) {
-      if (lastSlash !== index - 1 && dots !== 1) {
-        if (dots === 2) {
-          if (
-            result.length < 2 ||
-            lastSegmentLength !== 2 ||
-            result.codePointAt(result.length - 1) !== CHAR_DOT ||
-            result.codePointAt(result.length - 2) !== CHAR_DOT
-          ) {
-            if (result.length > 2) {
-              const lastSlashIndex = result.lastIndexOf("/");
-              if (lastSlashIndex === -1) {
-                result = "";
-                lastSegmentLength = 0;
-              } else {
-                result = result.slice(0, lastSlashIndex);
-                lastSegmentLength = result.length - 1 - result.lastIndexOf("/");
-              }
-              lastSlash = index;
-              dots = 0;
-              continue;
-            } else if (result.length > 0) {
-              result = "";
-              lastSegmentLength = 0;
-              lastSlash = index;
-              dots = 0;
-              continue;
-            }
-          }
-          if (shouldAllowAboveRoot) {
-            result += result.length > 0 ? `/..` : "..";
-            lastSegmentLength = 2;
-          }
-        } else {
-          if (result.length > 0) {
-            result += `/${path.slice(lastSlash + 1, index)}`;
-          } else {
-            result = path.slice(lastSlash + 1, index);
-          }
-          lastSegmentLength = index - lastSlash - 1;
-        }
-      }
-      lastSlash = index;
-      dots = 0;
-    } else if (code === CHAR_DOT && dots !== -1) {
-      dots += 1;
-    } else {
-      dots = -1;
-    }
-  }
-  return result;
-};
+const { posix } = path;
 
 export const join = (pathA: string, pathB: string, ...extraPaths: string[]) => {
-  const paths = [pathA, pathB, ...extraPaths];
-  if (paths.length === 0) {
-    return ".";
-  }
-
-  let joined: string | undefined;
-  for (const path of paths) {
-    if (path.length > 0) {
-      if (joined === undefined) {
-        joined = path;
-      } else {
-        joined += "/" + path;
-      }
-    }
-  }
-
-  if (joined === undefined) {
-    return ".";
-  }
-
-  return normalize(joined);
+  return ensureNoSlash(posix.join(pathA, pathB, ...extraPaths));
 };
 
 export const resolve = (path: string, ...extraPaths: string[]) => {
-  let resolvedPath = "";
-  let isAbsolute = false;
-
-  const paths = [path, ...extraPaths];
-  for (let index = paths.length - 1; index >= -1 && !isAbsolute; index--) {
-    const path = index >= 0 ? paths[index] : process.cwd();
-    if (path.length === 0) {
-      continue;
-    }
-
-    resolvedPath = path + "/" + resolvedPath;
-    isAbsolute = path.codePointAt(0) === CHAR_FORWARD_SLASH;
-  }
-
-  resolvedPath = normalizeString(resolvedPath);
-  if (isAbsolute) {
-    return `/${resolvedPath}`;
-  }
-  return resolvedPath.length > 0 ? resolvedPath : ".";
+  return ensureNoSlash(posix.resolve(path, ...extraPaths));
 };
 
 export const relative = (fromPath: string, toPath: string) => {
-  if (fromPath === toPath) {
-    return "";
-  }
-
-  fromPath = absolute(fromPath);
-  toPath = absolute(toPath);
-
-  if (fromPath === toPath) {
-    return "";
-  }
-
-  const fromStart = 1;
-  const fromEnd = fromPath.length;
-  const fromLength = fromEnd - fromStart;
-  const toStart = 1;
-  const toLength = toPath.length - toStart;
-
-  const length = fromLength < toLength ? fromLength : toLength;
-  let lastCommonSeparator = -1;
-  let index = 0;
-  for (; index < length; index++) {
-    const fromCode = fromPath.codePointAt(fromStart + index);
-    if (fromCode !== toPath.codePointAt(toStart + index)) {
-      break;
-    } else if (fromCode === CHAR_FORWARD_SLASH) {
-      lastCommonSeparator = index;
-    }
-  }
-  if (index === length) {
-    if (toLength > length) {
-      if (toPath.codePointAt(toStart + index) === CHAR_FORWARD_SLASH) {
-        return toPath.slice(toStart + index + 1);
-      }
-      if (index === 0) {
-        return toPath.slice(toStart + index);
-      }
-    } else if (fromLength > length) {
-      if (fromPath.codePointAt(fromStart + index) === CHAR_FORWARD_SLASH) {
-        lastCommonSeparator = index;
-      } else if (index === 0) {
-        lastCommonSeparator = 0;
-      }
-    }
-  }
-
-  let out = "";
-  for (index = fromStart + lastCommonSeparator + 1; index <= fromEnd; ++index) {
-    if (index === fromEnd || fromPath.codePointAt(index) === CHAR_FORWARD_SLASH) {
-      out += out.length === 0 ? ".." : "/..";
-    }
-  }
-
-  return `${out}${toPath.slice(toStart + lastCommonSeparator)}`;
+  return ensureNoSlash(posix.relative(fromPath, toPath));
+};
+export const absolute = (path: string): string => {
+  return resolve(path);
 };
 
 export const normalize = (path: string) => {
-  if (path.length === 0) {
-    return ".";
-  }
-  const isAbsolute = path.codePointAt(0) === CHAR_FORWARD_SLASH;
-  if (isAbsolute && path.length === 1) {
-    return path;
-  }
-  path = normalizeString(path);
-  return isAbsolute ? `/${path}` : path || ".";
+  return ensureNoSlash(posix.normalize(path));
 };
 
 export const split = (path: string) => {
@@ -216,12 +52,24 @@ export const extension = (path: string, maxDotsInExtension = 1): string => {
   }
 };
 
-export const ensureSlash = (path: string): string => {
-  return path.endsWith("/") ? path : path + "/";
+export const ensureNoSlash = (path: string, shouldRemoveRootSlash = false): string => {
+  let trailingSlashStartIndex = 1;
+  for (let i = path.length - 1; i >= 1; i--) {
+    if (path[i] !== "/") {
+      trailingSlashStartIndex = i + 1;
+      break;
+    }
+  }
+  const newPath = path.slice(0, trailingSlashStartIndex);
+  if (newPath === "/") {
+    return shouldRemoveRootSlash ? "" : newPath;
+  } else {
+    return newPath;
+  }
 };
 
-export const absolute = (path: string): string => {
-  return resolve(path);
+export const ensureSlash = (path: string): string => {
+  return path.endsWith("/") ? path : path + "/";
 };
 
 export const up = (path: string, throwIfRoot = false): string => {
